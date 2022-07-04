734a370e69a4a9eaf9bc03f8108a1f6eaf11e97b
ugursahin <ugur_sahin_18@hotmail.com>
2022-07-04 21:47:36 +0300

    Ogrenci Class'ı ikinci bölüm yapıldı.

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya");
+
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
